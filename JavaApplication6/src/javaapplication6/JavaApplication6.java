5795ca4fc4486e2d1538a001033f684b788920dc
chikilinder <chikilinder_34@hotmail.com>
2016-01-18 19:21:57 -0600

    Update JavaApplication6.java

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -17,6 +17,8 @@ public class JavaApplication6 {
     public static void main(String[] args) {
         
        System.out.println("hello prof");
+              System.out.println("un punto");
+
         // TODO code application logic here
     }
     
